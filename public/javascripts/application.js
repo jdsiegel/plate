795d9fbc54afddb8af5b2a286875188256f47773
Jeff Siegel <jdsiegel@gmail.com>
2010-11-12 01:10:55 -0700

    CSS drop down menus. supports IE8/7
    IE6 requires testing still

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,16 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(document).ready(function () {
+    $("li.clickdown > a").live('click', function() {
+        $(this).parent().toggleClass("active");
+        return false;
+    });
+    $("li.clickdown").live('mouseleave', function() { 
+        //$(this).removeClass("active");
+        return false;
+    });
+    $("body").live('click', function() {
+        $("li.clickdown.active").removeClass("active");
+    });
+});
