a83e9d9178f42bd009d3480fe322690864fbb3d7
Jeff Siegel <jdsiegel@gmail.com>
2010-11-16 00:42:28 -0700

    added modernizr, HTML5 tags
    ie6 now loads IE7.js. Best I can do for now

--- public/javascripts/ie.js
+++ public/javascripts/ie.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $(".links ul:first-child, .nav ul > li:first-child").addClass("first");
-  $(".links ul:last-child, .nav ul > li:last-child").addClass("last");
-  $(".nav ul > li:not(.clickdown)").addClass("dropdown");
+  $(".links ul:first-child, nav ul > li:first-child").addClass("first");
+  $(".links ul:last-child, nav ul > li:last-child").addClass("last");
+  $("nav ul > li:not(.clickdown)").addClass("dropdown");
 });
